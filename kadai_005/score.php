99c5341c378f4221c23246613f2d30d667d98625
matsu-ume <145898367+matsu-ume@users.noreply.github.com>
2023-12-17 20:32:18 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lamg="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>四則演算</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    echo ( 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60 ) / 10;
+    ?>
+  </p>
+</body>
+
+</html>
