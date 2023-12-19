9625e4eb47d63842f0ca7ce5900ddf1bb1a14c2e
matsu-ume <145898367+matsu-ume@users.noreply.github.com>
2023-12-19 21:35:13 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $data_a = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+    print_r($data_a);
+    ?>
+  </p>
+</body>
+</html>
